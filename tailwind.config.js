b5431c563191077b645d3db34c0e2ba1f9939fa8
lordwhitefire <victormakuo2002@gmail.com>
2023-05-23 22:36:38 +0100

    finished the design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+//  darkMode: media, // or 'media' or 'class'
+  content: ["./storage/**/*.{html,js}",
+'./index.html'],
+ theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
